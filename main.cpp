d096ab8cd17e7b1073939e1d3ea0cbf260a4b494
Yrish-1 <pineyrish@gmail.com>
2026-01-14 09:34:35 +0800

    1st

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
-using namespace std;
+#include <iostream>// this is the reason why we are able to use cout and ciin
+using namespace std; //allows us to use the variables inside the standard library para hindi na mag prompt ng mahabang syntax kumbaag siya yung short cut
 
-int main() {
-    cout<<"Hello, cutie!"<<endl;
+int main() { // this is the function bale yumg bracket lahat ng nasa loob ng bracket na yan ay ieexecute ng main function
+    cout<<"Hello, cutie!"<<endl; //ginagamit naman ang cout kasama ng << insertion operator para mag insert ng something variable na ididisplay yan yung nakikita natin na dinidisplay sa labas ng code
+    return 0;
 }
+
+//
